dfb4af94dacf208a345d74413a36ed50c96fb438
luis.celedon <luis.celedon@usach.cl>
2014-06-10 23:31:04 -0400

    Agregado controlador y vista para el home, aunque vacios

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :transaccions
 
   resources :tipo_transaccions
@@ -59,7 +61,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
